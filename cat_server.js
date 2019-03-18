b77d1989417dd7575a8f77f623305dbd30524296
Nico De Witte <nico.dewitte@outlook.com>
2019-03-18 13:24:35 +0100

    Refactor channel to own file

--- cat_server.js
+++ cat_server.js
@@ -1,52 +1,9 @@
 const mqtt = require('mqtt');
 const Player = require('./lib/player.js')
 const Card = require('./lib/card.js');
+const Channel = require('./lib/channel.js');
 
 // Needs to be refactored to own file
-class Channel {
-
-  constructor(name) {
-    this.name = name;
-    this.players = {};
-    this.playedCards = [];
-    this.question = "Who looks like what?";
-  }
-
-  add_player(player) {
-    if (Object.keys(this.players).length < 5 && !this.players[player.get_name()]) {
-      console.log("Player " + player.get_name() + " joining the channel");
-      this.players[player.get_name()] = player;
-      return true;
-    }
-    return false;
-  }
-
-  get_player(name) {
-    return this.players[name];
-  }
-
-  get_players() {
-    return this.players;
-  }
-
-  get_name() {
-    return this.name;
-  }
-
-  add_card(card) {
-    // Check if player not already played card
-    if (!this.playedCards.filter( c => c.owner.name === card.owner.name ).length) {
-      this.playedCards.push(card);
-    } else {
-      console.log("Player " + card.owner.name + " already played card");
-    }
-  }
-
-  get_cards() {
-    return this.playedCards;
-  }
-}
-
 class Game {
 
   constructor(mqttHost) {
